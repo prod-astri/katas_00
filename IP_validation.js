cd6f1f4c987dd076102ba0f322108ef631fdb576
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-11-27 15:58:01 +0100

    IP_validations solved

--- IP_validation.js
+++ IP_validation.js
@@ -1,11 +1,24 @@
-function isValidIP(str) {
-  return false;
-}
-
 let inputs = [
-  ['1.2.3'],
-  ['127.1.1.0']
+  ' 1.2.3.5',
+  '127.1.1.0',
+  '555.1.1.1',
+  '001.11.001.1',
+  'bob.ddd.ddd',
+  '-0.0.0.0',
+  '111.44.55.3'
 ]
+
+function isValidIP(str) {
+  let numbersArray = str.split('.')
+  
+  if (numbersArray.length !== 4) return false;
+  for (let num of numbersArray){
+    if (/^\d+$/.test(num) == false) return false;
+    if (num.length > 1 && num[0] === '0') return false;
+    if (!(Number(num) >= 0 && Number(num) <= 255)) return false;
+  }
+  return true;
+}
 
 for (let IP of inputs){ 
   console.log(isValidIP(IP))
