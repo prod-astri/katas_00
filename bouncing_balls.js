e0af3cafba2c0553711e5d15b65eb117faed3e37
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-11-28 13:40:08 +0100

    bouncing_balls pass test, fail submission?

--- bouncing_balls.js
+++ bouncing_balls.js
@@ -1,5 +1,15 @@
 function bouncingBall(h, bounce, window) {
-
+  if (h <= 0 || bounce <= 0 || bounce > 1 || window >= h) {
+    return -1;
+  } else {
+    let res = 1;
+    let newHeight = h * bounce;
+    while (newHeight > window) {
+      res += 2;
+      newHeight = newHeight * bounce;
+    }
+    return res;
+  }
 }
 
 console.log(bouncingBall(3.0, 0.66, 1.5));
