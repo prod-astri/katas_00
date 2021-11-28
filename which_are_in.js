bac1fd2747e1d5c569725672effeac582cba06de
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-11-28 13:02:27 +0100

    which_are_in solved

--- which_are_in.js
+++ which_are_in.js
@@ -1,8 +1,13 @@
-const a1 = ["arp", "live", "strong", 'xxx']
+const a1 = ["arp", "live", "strong", 'arp', 'xxx']
 const a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 function inArray(array1,array2){
-
+    return [...new Set(array1.filter(substr => {
+      for (let str of array2){
+        if (str.includes(substr)) return true;
+      }
+      return false;
+    }).sort())]
   }
 
 console.log(inArray(a1, a2))
