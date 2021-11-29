42ffe9617ad386836ec4a061bd07895505ccdf2e
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-11-29 13:30:55 +0100

    product_of_consecutive_fib_numbers solved

--- product_of_consecutive_fib_numbers.js
+++ product_of_consecutive_fib_numbers.js
@@ -1,5 +1,15 @@
 function productFib(prod){
-
+  let multiplied = 0;
+  let n = 0;
+  let n1 = 1;
+  let sum = 0;
+  while (multiplied < prod){
+    sum = n1 + n;
+    n = n1;
+    n1 = sum;
+    multiplied = n * n1
+  }
+  return [n, n1, (multiplied === prod)]
 }
 
 productFib(40)
