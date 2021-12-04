a7e75bc70fd50b51150b7e9852bfe0def3a01c61
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-12-04 13:35:48 +0100

    perfect_power solved

--- perfect_power.js
+++ perfect_power.js
@@ -1,11 +1,27 @@
 // https://www.codewars.com/kata/54d4c8b08776e4ad92000835/train/javascript
 
-var isPP = function(n){
+function isPP(a){
+    let x = 2;
+    let n = 2;
+    const y = a;
+
+    while (x * x <= y){
+        while (x ** n <= y){
+            console.log('x:', x, "n:", n)
+            if (x ** n === y){
+                return [x, n];
+            }
+            x++
+        };
+        n++
+        if (2 ** n <= y) x = 2
+        else return null
+    };
     return null; // fix me
   }
 
-console.log(4, isPP(4));
-console.log(9, isPP(9));
-console.log(5, isPP(5));
-console.log(100, isPP(100));
-console.log(144, isPP(144));
+// console.log('4:', isPP(4));
+// console.log('9:', isPP(9));
+// console.log('5:', isPP(5));
+// console.log('100:', isPP(100));
+console.log('146:', isPP(146));
